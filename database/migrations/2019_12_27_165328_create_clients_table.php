facf209f3cf9102cd9e67956f1150e07db9a81ab
Humberto Arcaya <betoarcaya@gmail.com>
2019-12-27 13:02:30 -0400

    generando migraciones nuevas para la tabla de concesionarios y la lista de los clientes por concesionarios

---
+++ database/migrations/2019_12_27_165328_create_clients_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateClientsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('clients', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('user_id');
+            $table->unsignedBigInteger('car_dealer_id');
+            $table->timestamps();
+
+            $table->foreign('user_id')
+                  ->references('id')
+                  ->on('users');
+            $table->foreign('car_dealer_id')
+                  ->references('id')
+                  ->on('car_dealerships');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('clients');
+    }
+}
