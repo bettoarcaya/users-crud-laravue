cfbc147fe368e487e0341956dec2844e570a7de5
Humberto Arcaya <betoarcaya@gmail.com>
2019-12-27 13:28:19 -0400

    agregagndo los items corresondientes en el navbar

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -18,6 +18,8 @@
       <ul class="navbar-nav ml-auto">
         <!-- Authentication Links -->
         @if( Auth::check() )
+          <li class="nav-item"><a class="nav-link" href="javascript:void(0)"> Clientes </a></li>
+          <li class="nav-item"><a class="nav-link" href="javascript:void(0)"> Reportes </a></li>
           <li class="nav-item dropdown">
             <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
               {{ Auth::user()->name }} <span class="caret"></span>
